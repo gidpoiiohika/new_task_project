c93da36e56da3720014658f1d987a39c2af7af0e
Yaroslav <adacer73044210@gmail.com>
2022-10-03 23:23:08 +0300

    add User

---
+++ db/migrate/20221003201730_devise_create_users.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :email,              null: false, default: "", index: { unique: true }
+      t.string :encrypted_password, null: false, default: ""
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name,          null: false, default: ""
+      t.datetime :remember_created_at
+
+      t.timestamps null: false
+    end
+  end
+end
