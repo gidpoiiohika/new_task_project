8efe1cc43f71bb0c87545aacb9dca990baeb7078
Yaroslav <adacer73044210@gmail.com>
2022-10-05 05:27:17 +0300

    new_task

--- app/models/role.rb
+++ app/models/role.rb
@@ -7,7 +7,7 @@ class Role < ApplicationRecord
   
 
   validates :resource_type,
-            inclusion: { :in => Rolify.resource_types },
+            inclusion: { in: Rolify.resource_types },
             allow_nil: true
 
   scopify
